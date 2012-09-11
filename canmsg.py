37c666e888dbc07bf838e10aa35157d41e38e008
Per Forser <pf@binar.se>
2012-09-11 14:04:58 +0000

    blabla

--- canmsg.py
+++ canmsg.py
@@ -1,58 +1,6 @@
 #!/usr/bin/env python
 import re
 
-canMSG_RTR = 0x0001      # Message is a remote request
-canMSG_STD = 0x0002      # Message has a standard ID
-canMSG_EXT = 0x0004      # Message has an extended ID
-canMSG_WAKEUP = 0x0008      # Message to be sent / was received in wakeup mode
-canMSG_NERR = 0x0010      # NERR was active during the message
-canMSG_ERROR_FRAME = 0x0020      # Message is an error frame
-canMSG_TXACK = 0x0040      # Message is a TX ACK (msg is really sent)
-canMSG_TXRQ = 0x0080      # Message is a TX REQUEST (msg is transfered to the chip)
-canMSGERR_HW_OVERRUN = 0x0200      # HW buffer overrun
-canMSGERR_SW_OVERRUN = 0x0400      # SW buffer overrun
-canMSGERR_STUFF = 0x0800      # Stuff error
-canMSGERR_FORM = 0x1000      # Form error
-canMSGERR_CRC = 0x2000      # CRC error
-canMSGERR_BIT0 = 0x4000      # Sent dom, read rec
-canMSGERR_BIT1 = 0x8000      # Sent rec, read dom
-
-flags_inv = {
-        0x0001: 'canMSG_RTR',
-        0x0002: 'canMSG_STD',
-        0x0004: 'canMSG_EXT',
-        0x0008: 'canMSG_WAKEUP',
-        0x0010: 'canMSG_NERR',
-        0x0020: 'canMSG_ERROR_FRAME',
-        0x0040: 'canMSG_TXACK',
-        0x0080: 'canMSG_TXRQ',
-        0x0200: 'canMSGERR_HW_OVERRUN',
-        0x0400: 'canMSGERR_SW_OVERRUN',
-        0x0800: 'canMSGERR_STUFF',
-        0x1000: 'canMSGERR_FORM',
-        0x2000: 'canMSGERR_CRC',
-        0x4000: 'canMSGERR_BIT0',
-        0x8000: 'canMSGERR_BIT1',
-        }
-
-flag_texts = {
-        canMSG_RTR: 'RTR',
-        canMSG_STD: 'STD',
-        canMSG_EXT: 'EXT',
-        canMSG_WAKEUP: 'WAKEUP',
-        canMSG_NERR: 'NERR',
-        canMSG_ERROR_FRAME: 'ERROR_FRAME',
-        canMSG_TXACK: 'TXACK',
-        canMSG_TXRQ: 'TXRQ',
-        canMSGERR_HW_OVERRUN: 'HW_OVERRUN',
-        canMSGERR_SW_OVERRUN: 'SW_OVERRUN',
-        canMSGERR_STUFF: 'STUFF',
-        canMSGERR_FORM: 'FORM',
-        canMSGERR_CRC: 'CRC',
-        canMSGERR_BIT0: 'BIT0',
-        canMSGERR_BIT1: 'BIT1',
-        }
-
 class Data(list):
     __fmt1 = '{0:02X}'
 
@@ -146,6 +94,8 @@ class CanMsg(object):
         for i in range(len(self._data)):
             if self._data[i] != other._data[i]:
                 return False
+        if self._extened != other._extened:
+            return False
         return True
 
     def __ne__(self, other):
@@ -155,3 +105,4 @@ if __name__ == '__main__':
     m = CanMsg()
     print m.__sizeof__()
     print dir(m)
+
