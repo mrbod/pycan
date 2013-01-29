d5659a815d23eceb561b037ff65fa2c117996306
Per Forser <per.forser@gmail.com>
2013-01-29 21:35:45 +0100

    some tk tests

--- tk.py
+++ tk.py
@@ -20,7 +20,7 @@ class Ch(object):
     def run(self):
         while self.running:
             self.q.put(str(time.time()))
-            time.sleep(0.01 * random.random())
+            time.sleep(0.1 * random.random())
 
     def read(self):
         if not self.running:
@@ -31,59 +31,64 @@ class Ch(object):
             return None
 
 class App(object):
-    def __init__(self, master):
-        self.master = master
-        bf = Frame(master)
-        bf.pack(side=BOTTOM, fill=Y)
-        self.button = Button(bf, text="Quit", fg="red", command=self.quit)
+    def __init__(self):
+        self.root = Tk()
+        # buttons
+        bf = Frame(self.root)
+        bf.pack(side=BOTTOM)
+        self.button = Button(bf, text="QUIT", fg="red", command=self.quit)
         self.button.pack(side=LEFT)
         p = Button(bf, text="Pause", command=self.auto_scroll)
-        p.pack()
-        frame = Frame(master)
+        p.pack(side=LEFT)
+        # text view
+        frame = Frame(self.root)
         frame.pack(expand=True, fill="both")
+        self.iw = Text(frame)
+        self.iw.pack(side=LEFT, expand=False, fill="both")
         self.text = Text(frame)
         self.text.pack(side=LEFT, expand=True, fill="both")
         scrbar = Scrollbar(frame)
         scrbar.pack(side=RIGHT, fill=Y)
         scrbar.config(command=self.text.yview)
         self.text.config(yscrollcommand = scrbar.set)
-        self._auto_scroll = False
+        self.auto_scroll = False
         self.row = 0
-        self.ch = cdcchannel.CDCChannel(0, "localhost", 5555)
-        self.ch.logger = self
-        self.master.after(100, self.poll)
+        self.ch =  Ch()
+        #cdcchannel.CDCChannel(0, "slacker", 5555, msg_class=stcan.StCanMsg)
+        self.root.after(100, self.poll)
+
+    def mainloop(self):
+        try:
+            self.root.mainloop()
+        finally:
+            self.end = True
 
     def auto_scroll(self):
-        self._auto_scroll = not self._auto_scroll
+        self.auto_scroll = not self.auto_scroll
 
     def quit(self):
         self.ch.close()
-        self.master.quit()
+        self.root.quit()
 
     def poll(self):
         m = self.ch.read()
         while m:
             self.log(m)
             m = self.ch.read()
-        if not self._auto_scroll:
+        if not self.auto_scroll:
             self.text.see(END)
-        self.master.after(500, self.poll)
+        self.root.after(100, self.poll)
 
     def info(self, row, m):
-        self.text.insert(END, "{0}\n".format(str(m)))
+        self.iw.insert(END, "{0}\n".format(str(m)))
 
     def log(self, m):
         self.row += 1
         self.text.insert(END, "{1:5d}: {0}\n".format(str(m), self.row))
 
 def main():
-    root = Tk()
-    app = App(root)
-    try:
-        root.mainloop()
-    finally:
-        app.end = True
-    sys.exit()
+    app = App()
+    app.mainloop()
 
 if __name__ == '__main__':
     try:
