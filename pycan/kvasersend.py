0c59fc62d7679da5584f0f01165038d1c55814ae
Per Forser <per.forser@binar.se>
2015-06-03 14:19:43 +0200

    Refined kvasersend interface

--- pycan/kvasersend.py
+++ pycan/kvasersend.py
@@ -4,6 +4,11 @@ import time
 import pycan.kvaser as kvaser
 import pycan.canmsg as canmsg
 
+def wait(dt):
+    dt -= 0.001
+    if dt > 0.0:
+        time.sleep(dt - 0.001)
+
 def send_file(channel, f, extended, interval):
     if interval is None:
         for m in canmsg.translate(f, True, extended):
@@ -11,12 +16,25 @@ def send_file(channel, f, extended, interval):
     else:
         for m in canmsg.translate(f, False, extended):
             channel.write(m)
-            time.sleep(interval)
+            wait(interval)
+    time.sleep(0.1)
 
-def send_message(channel, interval, m):
-    while True:
+def send_message(channel, count, interval, m):
+    if count is None:
         channel.write(m)
-        time.sleep(interval)
+        while True:
+            wait(interval)
+            channel.write(m)
+    else:
+        cnt = 0
+        if cnt < count:
+            channel.write(m)
+            cnt += 1
+        while cnt < count:
+            wait(interval)
+            channel.write(m)
+            cnt += 1
+    time.sleep(0.1)
 
 def execute(args):
     if not args.pure:
@@ -24,16 +42,19 @@ def execute(args):
     ch = kvaser.KvaserCanChannel(channel=args.channel, bitrate=args.bitrate)
     if args.filename:
         if args.filename == '-':
-            send_file(ch, sys.stdin, args.extended, args.interval)
+            f = sys.stdin
         else:
-            send_file(ch, open(args.filename), args.extended, args.interval)
+            f = open(args.filename)
+        send_file(ch, f, args.extended, args.interval)
     else:
         m = canmsg.CanMsg()
         m.can_id = args.canid
         m.extended = args.extended
+        if len(args.data) > 8:
+            raise Exception('More than 8 data bytes given')
         m.data = [int(x, 0) for x in args.data]
         interval = (args.interval is None) and 0.1 or args.interval
-        send_message(ch, interval, m)
+        send_message(ch, args.count, interval, m)
 
 def main():
     import argparse
@@ -44,14 +65,16 @@ def main():
             , help='select CAN bitrate, default=%(default)s')
     p.add_argument('-p', '--pure', action='store_true'
             , help='do not use the default BiCAN format')
+    p.add_argument('-i', '--canid', type=int, default=0
+            , help='send message(s) with specified CAN id, default=%(default)s')
     p.add_argument('-e', '--extended', action='store_true'
             , help='send extended message(s)')
     p.add_argument('-d', '--data', nargs='*', default=[]
-            , help='send message(s) with specified data')
-    p.add_argument('-i', '--interval', type=float, default=None
+            , help='send message(s) with specified data, default=%(default)s')
+    p.add_argument('-t', '--interval', type=float, default=None
             , help='send message(s) with specified interval')
-    p.add_argument('-I', '--canid', type=int, default=0
-            , help='send message(s) with specified CAN id')
+    p.add_argument('-C', '--count', type=int, default=None
+            , help='send COUNT message(s), sends forever if not given')
     p.add_argument('filename', nargs='?'
             , help='file with CAN data to replay, timing from file if not --interval is specified')
     args = p.parse_args()
